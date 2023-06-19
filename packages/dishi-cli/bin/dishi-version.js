20f40a402c4fdf1078b0cce92925d5ea1e582196
uikoo9 <uikoo9@qq.com>
2023-06-19 19:58:20 +0800

    feat(dishi): del dishi-cli

--- packages/dishi-cli/bin/dishi-version.js
+++
@@ -1,8 +0,0 @@
-// qiao
-const qiao = require('../src/util/qiao.js');
-
-// package
-const pkg = require('../package.json');
-
-// cmd for version
-qiao.cli.cmd.version(pkg.version, '-v, --version').usage('<command> [options]');
--- packages/dishi-cli/src/util/qiao.js
+++
@@ -1,8 +0,0 @@
-const qiao = {};
-qiao.ajax = require('qiao-ajax');
-qiao.cli = require('qiao-cli');
-qiao.config = require('qiao-config')();
-qiao.console = require('qiao-console');
-qiao.log = require('./log.js');
-
-module.exports = qiao;
