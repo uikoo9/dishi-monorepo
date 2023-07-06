bd76678a9272cf8be47fc0ce320c1d01d316d990
uikoo9 <uikoo9@qq.com>
2023-07-06 14:48:19 +0800

    feat(dishi): rollup pack ok

--- packages/dishi-electron/pack/gulp/pack-build.js
+++ packages/dishi-electron/pack/gulp/pack-build.js
@@ -1,9 +1,6 @@
 // gulp
 const { series } = require('gulp');
 
-// cp
-const { cp } = require('qiao-file');
-
 // exec
 const util = require('util');
 const exec = util.promisify(require('node:child_process').exec);
@@ -12,10 +9,9 @@ const exec = util.promisify(require('node:child_process').exec);
 const buildRenderer = async () => await exec('cd ../../../../ && npm run build');
 
 // main
-const copyMain = async () => await cp('../../../dishi-main', '../../dist');
 const cmd = `cd ../../dist && npm i ${process.arch === 'arm64' ? '--target_arch=arm64' : ''}`;
 console.log(`cmd:${cmd}`);
 const npmInstallMain = async () => await exec(cmd);
 
 // build
-exports.build = series(buildRenderer, copyMain, npmInstallMain);
+exports.build = series(buildRenderer, npmInstallMain);
